5da55e42903d99cd8f756fd7b476cc522671edb9
tamB2203579 <tamB2203579@student.ctu.edu.vn>
2024-09-21 20:52:48 +0700

    fix sign up screen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, Dimensions, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard} from 'react-native'
+import { View, Text, Image, Dimensions, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import React, { useState } from 'react'
 import UserTextInput from '../components/UserTextInput';
 import { useNavigation } from '@react-navigation/native';
@@ -31,7 +31,7 @@ const SignupScreen = () => {
     }
 
     const handleSignUp = () => {
-        if(getEmailValidationStatus && email.length!==""){
+        if (getEmailValidationStatus && email.length !== "") {
             createUserWithEmailAndPassword(firebaseAuth, email, password)
                 .then((cred) => {
                     const data = {
@@ -51,114 +51,109 @@ const SignupScreen = () => {
     }
 
     return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <View className="flex-1 justify-start items-center">
-        <Image 
-        source={require("../assets/bg.png")}
-        resizeMode="cover"
-        className="h-96"
-        style={{width: screenWidth}}/>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View className="flex-1 justify-start items-center">
+                <Image
+                    source={require("../assets/bg.png")}
+                    resizeMode="cover"
+                    className="h-60"
+                    style={{ width: screenWidth }} />
 
-        {isAvatarMenu && (
-            <>
-                {/* list of avatars selection */}
-                <View className="absolute inset-0 z-10" style={{width: screenWidth, height: screenHeight}}>
-                    <ScrollView>
-                        <BlurView 
-                            className="w-full h-full px-4 py-16 flex-row flex-wrap items-center justify-evenly" 
-                            tint="light" 
-                            intensity={40}
-                            style={{width: screenWidth, height: screenHeight}}
-                        >
-                            {avatars?.map((item) => {
-                                return(                            
-                                <TouchableOpacity onPress={() => handleAvatar(item)}
-                                key={item._id} 
-                                className="w-20 m-3 h-20 p-1 rounded-full border-2 border-primary relative">
-                                    <Image source={{uri: item.image.asset.url}} 
-                                    className="w-full h-full"
-                                    resizeMode="contain"/>
-                                </TouchableOpacity>);
-                            })}
-                        </BlurView>
-                    </ScrollView>
-                </View>
-            </>
-        )}
+                {isAvatarMenu && (
+                    <>
+                        {/* list of avatars selection */}
+                        <View className="absolute inset-0 z-10" style={{ width: screenWidth, height: screenHeight }}>
+                            <ScrollView>
+                                <BlurView
+                                    className="w-full h-full px-4 py-16 flex-row flex-wrap items-center justify-evenly"
+                                    tint="light"
+                                    intensity={40}
+                                    style={{ width: screenWidth, height: screenHeight }}
+                                >
+                                    {avatars?.map((item) => {
+                                        return (
+                                            <TouchableOpacity onPress={() => handleAvatar(item)}
+                                                key={item._id}
+                                                className="w-20 m-3 h-20 p-1 rounded-full border-2 border-primary relative">
+                                                <Image source={{ uri: item.image.asset.url }}
+                                                    className="w-full h-full"
+                                                    resizeMode="contain" />
+                                            </TouchableOpacity>);
+                                    })}
+                                </BlurView>
+                            </ScrollView>
+                        </View>
+                    </>
+                )}
 
-        {/* Main View */}
-        <View className="w-full h-full bg-white rounded-tl-[90px] -mt-44 flex items-center justify-start py-6 px-6 space-y-6">
-        {/* <Image 
-        source={require("./images/logo.png")}
-        className="h-16 w-16"
-        resizeMode="contain"
-        /> */}
+                {/* Main View */}
+                <View className="w-full h-full bg-white rounded-tl-[90px] -mt-44 flex items-center justify-start py-6 px-6 space-y-6">
 
-        <Text className="text-primaryText text-xl font-semibold -mb-4">Join with us!</Text>
+                    <Text className="text-primaryText text-xl font-semibold -mb-4">Join with us!</Text>
 
-        {/* avatar section */}
-        <View className="w-full flex items-center justify-center relative -mb-4">
-            <TouchableOpacity onPress={() => setIsAvatarMenu(true)}
-            className="w-20 m-1.5 h-20 p-1 rounded-full border-2 relative" style={{borderColor: "#f48c06"}}>
-                <Image source={{uri: avatar}} className="w-full h-full" resizeMode="contain"/>
-                <View className="w-6 h-6 bg-primary rounded-full absolute top-0 right-0 flex items-center justify-center">
-                    <MaterialIcons name="edit" size={18} color="#fff"/>
-                </View>
-            </TouchableOpacity>
-        </View>
-        
-        <View className="w-full flex items-center justify-center">
-            {/* full name */}
-            <UserTextInput 
-            placeholder="Full Name" 
-            isPass={false} 
-            setStateValue={setName}
-            />
+                    {/* avatar section */}
+                    <View className="w-full flex items-center justify-center relative -mb-4">
+                        <TouchableOpacity onPress={() => setIsAvatarMenu(true)}
+                            className="w-20 m-1.5 h-20 p-1 rounded-full border-2 relative" style={{ borderColor: "#f48c06" }}>
+                            <Image source={{ uri: avatar }} className="w-full h-full" resizeMode="contain" />
+                            <View className="w-6 h-6 bg-primary rounded-full absolute top-0 right-0 flex items-center justify-center">
+                                <MaterialIcons name="edit" size={18} color="#fff" />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
 
-            {/* phone number */}
-            <UserTextInput 
-            placeholder="Phone Number" 
-            isPass={false} 
-            setStateValue={setPhoneNum}
-            />
+                    <View className="w-full flex items-center justify-center">
+                        {/* full name */}
+                        <UserTextInput
+                            placeholder="Full Name"
+                            isPass={false}
+                            setStateValue={setName}
+                        />
+
+                        {/* phone number */}
+                        <UserTextInput
+                            placeholder="Phone Number"
+                            isPass={false}
+                            setStateValue={setPhoneNum}
+                        />
 
 
-            {/* email */}
-            <UserTextInput 
-            placeholder="Email" 
-            isPass={false} 
-            setStateValue={setEmail}
-            setGetEmailValidationStatus={setGetEmailValidationStatus}
-            />
+                        {/* email */}
+                        <UserTextInput
+                            placeholder="Email"
+                            isPass={false}
+                            setStateValue={setEmail}
+                            setGetEmailValidationStatus={setGetEmailValidationStatus}
+                        />
 
-            {!getPasswordValidationStatus && password.length > 0 && (
+                        {!getPasswordValidationStatus && password.length > 0 && (
                             <Text className="text-base text-red-600">Password must be at least 7 characters long</Text>
                         )}
 
-            {/* password */}
-            <UserTextInput 
-            placeholder="Password"
-            isPass={true} 
-            setStateValue={setPassword}
-            setGetPasswordValidationStatus={setGetPasswordValidationStatus}
-            />
+                        {/* password */}
+                        <UserTextInput
+                            placeholder="Password"
+                            isPass={true}
+                            setStateValue={setPassword}
+                            setGetPasswordValidationStatus={setGetPasswordValidationStatus}
+                        />
 
 
-            {/* login button */}
-            <TouchableOpacity onPress={handleSignUp} className="w-full px-4 py-2 rounded-xl bg-primary my-3 flex items-center justify-center">
-            <Text className="py-2 text-white text-xl font-semibold">Sign Up</Text>
-            </TouchableOpacity>
+                        {/* login button */}
+                        <TouchableOpacity onPress={handleSignUp} className="w-full px-4 py-2 rounded-xl bg-primary my-3 flex items-center justify-center">
+                            <Text className="py-2 text-white text-xl font-semibold">Sign Up</Text>
+                        </TouchableOpacity>
 
-            <View className="py-8 flex-row items-center justìy-center space-x-2">
-            <Text className="text-base text-primaryText">Have an account!</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                <Text className="text-base text-primaryBold" style={{fontWeight: "bold"}}>Login here</Text>
-            </TouchableOpacity>
+                        <View className="py-3 flex-row items-center justìy-center space-x-2 ">
+                            <Text className="text-base text-primaryText">Have an account!</Text>
+                            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                                <Text className="text-base text-primaryBold" style={{ fontWeight: "bold" }}>Login here</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
             </View>
-        </View>
-        </View>
-    </View>
-    </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback>
     )
 }
 
